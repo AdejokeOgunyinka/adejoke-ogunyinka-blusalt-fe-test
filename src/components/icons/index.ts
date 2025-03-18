15344f2cd1bd11ce89aedaa6f4fa83030c842513
Adejoke <adejokeogunyinka@enyata.com>
2025-03-18 17:02:47 +0100

    chore: setup font, icons and components

---
+++ src/components/icons/index.ts
@@ -0,0 +1,35 @@
+import {
+  Element3,
+  Timer,
+  SearchStatus1,
+  MobileProgramming,
+  Task,
+  Link1,
+  User,
+  SecurityUser,
+  Setting2,
+  Security,
+} from "iconsax-react";
+
+export * from "./arrow-up";
+export * from "./arrow-down";
+export * from "./bell";
+export * from "./bug-beetle";
+export * from "./counterclockwise-clock";
+export * from "./new-user";
+export * from "./sun";
+export * from "./wifi-signal";
+export * from "./window";
+
+export {
+  Element3 as DashboardIcon,
+  Timer as RequestsIcon,
+  SearchStatus1 as KYCIcon,
+  MobileProgramming as ReportsIcon,
+  Task as TasksIcon,
+  Link1 as AuditTrailIcon,
+  User as CustomersIcon,
+  SecurityUser as UsersIcon,
+  Setting2 as SettingsIcon,
+  Security as AdministrationIcon,
+};
